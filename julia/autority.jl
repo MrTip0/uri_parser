cd3a1067bee58c2346e9f89901fc0311f26c347f
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2025-01-07 10:28:38 +0000

    made julia ip length variable

--- julia/autority.jl
+++ julia/autority.jl
@@ -47,53 +47,81 @@ function ip0(uri :: URILib_structure, s :: String,
     elseif s[1] == '2'
         uri.host = string(uri.host, s[1])
         ip2(uri, s[2 : end], counter, path_fun)
+    elseif s[1] |> in(['3', '4', '5', '6', '7', '8', '9'])
+        uri.host = string(uri.host, s[1])
+        ip3(uri, s[2 : end], counter, path_fun)
     end
 end
 
 function ip1(uri :: URILib_structure, s :: String,
              counter :: Int, path_fun :: Function
              ) :: Union{URILib_structure, Nothing}
-    if length(s) == 0
+    if length(s) == 0 && counter == 3
+        uri
+    elseif length(s) == 0
         nothing
     elseif is_digit(s[1])
         uri.host = string(uri.host, s[1])
         ip3(uri, s[2 : end], counter, path_fun)
+    elseif s[1] == '.'
+        ip4(uri, s, counter, path_fun)
+    elseif counter == 3
+        ip6(uri, s, path_fun)
     end
 end
 
 function ip2(uri :: URILib_structure, s :: String,
              counter :: Int, path_fun :: Function
              ) :: Union{URILib_structure, Nothing}
-    if length(s) == 0
+    if length(s) == 0 && counter == 3
+        uri
+    elseif length(s) == 0
         nothing
     elseif s[1] |> in(['0', '1', '2', '3', '4'])
         uri.host = string(uri.host, s[1])
         ip3(uri, s[2 : end], counter, path_fun)
     elseif s[1] == '5'
         uri.host = string(uri.host, s[1])
+        ip5(uri, s[2 : end], counter, path_fun)
+    elseif s[1] |> in(['6', '7', '8', '9'])
+        uri.host = string(uri.host, s[1])
         ip4(uri, s[2 : end], counter, path_fun)
+    elseif s[1] == '.'
+        ip4(uri, s, counter, path_fun)
+    elseif counter == 3
+        ip6(uri, s, path_fun)
     end
 end
 
 function ip3(uri :: URILib_structure, s :: String,
              counter :: Int, path_fun :: Function
              ) :: Union{URILib_structure, Nothing}
-    if length(s) == 0
+    if length(s) == 0 && counter == 3
+        uri
+    elseif length(s) == 0
         nothing
     elseif is_digit(s[1])
         uri.host = string(uri.host, s[1])
-        ip5(uri, s[2 : end], counter, path_fun)
+        ip4(uri, s[2 : end], counter, path_fun)
+    elseif s[1] == '.'
+        ip4(uri, s, counter, path_fun)
+    elseif counter == 3
+        ip6(uri, s, path_fun)
     end
 end
 
 function ip4(uri :: URILib_structure, s :: String,
              counter :: Int, path_fun :: Function
              ) :: Union{URILib_structure, Nothing}
-    if length(s) == 0
+    if length(s) == 0 && counter == 3
+        uri
+    elseif length(s) == 0
         nothing
-    elseif s[1] |> in(['0', '1', '2', '3', '4', '5'])
+    elseif s[1] == '.' && counter < 3
         uri.host = string(uri.host, s[1])
-        ip5(uri, s[2 : end], counter, path_fun)
+        ip0(uri, s[2 : end], counter+1, path_fun)
+    elseif counter == 3
+        ip6(uri, s, path_fun)
     end
 end
 
@@ -104,12 +132,22 @@ function ip5(uri :: URILib_structure, s :: String,
         uri
     elseif length(s) == 0
         nothing
-    elseif s[1] == '.' && counter < 3
+    elseif s[1] |> in(['0', '1', '2', '3', '4', '5'])
         uri.host = string(uri.host, s[1])
-        ip0(uri, s[2 : end], counter+1, path_fun)
-    elseif s[1] == ':' && counter == 3
+        ip4(uri, s[2 : end], counter, path_fun)
+    elseif s[1] == '.'
+        ip4(uri, s, counter, path_fun)
+    elseif counter == 3
+        ip6(uri, s, path_fun)
+    end
+end
+
+function ip6(uri :: URILib_structure, s :: String,
+             path_fun :: Function
+             ) :: Union{URILib_structure, Nothing}
+    if s[1] == ':'
         parse_port(uri, s[2 : end], path_fun)
-    elseif s[1] == '/' && counter == 3
+    elseif s[1] == '/'
         choose_next_segment(uri, s[2 : end], path_fun)
     end
 end
